883900448eadce983ccd9cedd81a55596cc8eaad
shredthatgnar <benazir.temiralieva_2025@ucentralasia.org>
2022-05-04 03:29:27 +0500

    Added the option to run the program in cmd mode and gui mode

--- RUN.py
+++ RUN.py
@@ -2,3 +2,14 @@
 
 # PAYMENT CARD VALIDATOR RUN FILE
 APP_MODE = input("TYPE 'c' TO RUN IN COMMAND-LINE MODE, TYPE 'g' FOR GUI MODE OR ANY OTHER KEY TO EXIT THE APP: \n")
+
+if APP_MODE == "c":
+    # run CMD Mode file
+    import cmd_mode
+
+elif APP_MODE == "g":
+    # run GUI Mode file
+    import gui_mode
+else:
+    # quit the app
+    print("Quitting...")
