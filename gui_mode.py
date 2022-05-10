adf13d7dc57fbea3051a8907a23688e2fdc7023c
internic <m@internic.dev>
2022-05-10 17:22:26 +0600

    Initialising GUI mode: libraries

---
+++ gui_mode.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# PAYMENT CARD VALIDATOR GUI MODE
+import requests
+
+from requests import get
+from pathlib import Path
+
+# Explicit imports to satisfy Flake8
+from tkinter import Tk, Toplevel, Canvas, Entry, Text, Button, PhotoImage, messagebox
+
+pyglet for custom fonts
+from pyglet import font
