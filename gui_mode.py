bde4994eb989f8b32c0d8c3cddd456278ce5a682
internic <m@internic.dev>
2022-05-10 17:24:18 +0600

    Identifying paths

--- gui_mode.py
+++ gui_mode.py
@@ -9,5 +9,17 @@ from pathlib import Path
 # Explicit imports to satisfy Flake8
 from tkinter import Tk, Toplevel, Canvas, Entry, Text, Button, PhotoImage, messagebox
 
-pyglet for custom fonts
-from pyglet import font
+#pyglet for custom fonts
+#from pyglet import font
+
+# GUI settings
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path("./assets")
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
+
+# Using custom font didn't success because python couldn't import pyglet
+#font.add_file(relative_to_assets("Inter-Regular.ttf"))
+#Inter_Regular = font.load('Inter Regular')
